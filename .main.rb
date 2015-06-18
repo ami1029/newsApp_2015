761cf321bcd16e8146d4c95401c6fc7c4619657e
ami kanzaki <ami@ami-no-MacBook.local>
2015-06-18 15:12:29 +0900

    first commit

---
+++ .main.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+require 'rss'
+require 'json'
+
+get '/' do
+    @top = RSS::Parser.parse("http://news.livedoor.com/topics/rss/top.xml")
+    @dom = RSS::Parser.parse("http://news.livedoor.com/topics/rss/dom.xml")
+    @int = RSS::Parser.parse("http://news.livedoor.com/topics/rss/int.xml")
+    @eco = RSS::Parser.parse("http://news.livedoor.com/topics/rss/eco.xml")
+    @ent = RSS::Parser.parse("http://news.livedoor.com/topics/rss/ent.xml")
+    @spo = RSS::Parser.parse("http://news.livedoor.com/topics/rss/spo.xml")
+    @mov = RSS::Parser.parse("http://news.livedoor.com/topics/rss/52.xml")
+    @gourmet = RSS::Parser.parse("http://news.livedoor.com/topics/rss/gourmet.xml")
+    @love = RSS::Parser.parse("http://news.livedoor.com/topics/rss/love.xml")
+    @trend = RSS::Parser.parse("http://news.livedoor.com/topics/rss/trend.xml")
+    erb :index
+end
